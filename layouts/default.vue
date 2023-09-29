d64dd3ed89f5937e8db26a462e3f1fcb31a9f084
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-09-29 19:47:55 +0530

    fix: Nuxt components changed as nuxt

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="default-layout">
     <Header />
-    <Nuxt />
+    <nuxt />
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <main>
-    <MovieCard />
-    <MovieCard />
-    <MovieCard />
-    <MovieCard />
-    <MovieCard />
-    <MovieCard />
-    <MovieCard />
+    <MovieCard :movie="movie" />
+    <MovieCard :movie="movie" />
+    <MovieCard :movie="movie" />
+    <MovieCard :movie="movie" />
+    <MovieCard :movie="movie" />
+    <MovieCard :movie="movie" />
+    <MovieCard :movie="movie" />
+    <MovieCard :movie="movie" />
     <MovieCard />
   </main>
 </template>
@@ -16,5 +17,20 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  data() {
+    return {
+      movie: {
+        title: 'SAW X',
+        genres: ['1', '2'],
+        imageUrl:
+          'https://image.tmdb.org/t/p/w500/aQPeznSu7XDTrrdCtT5eLiu52Yu.jpg',
+        rating: 7.4,
+      },
+    }
+  },
 })
 </script>
+
+<style lang="scss" scoped>
+@import 'page.scss';
+</style>
