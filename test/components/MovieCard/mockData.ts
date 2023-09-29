dae54aecb1a6240531982500ee026d5599872c7b
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-09-29 22:39:26 +0530

    [P-001]: movie card component unit tests added

---
+++ test/components/MovieCard/mockData.ts
@@ -0,0 +1,8 @@
+export const MovieCardMockData = {
+  movie: {
+    title: 'Saw X',
+    genres: ['Crime', 'Horror', 'Thriller'],
+    imageUrl: 'https://image.tmdb.org/t/p/w500/aQPeznSu7XDTrrdCtT5eLiu52Yu.jpg',
+    rating: 7.4,
+  },
+}
