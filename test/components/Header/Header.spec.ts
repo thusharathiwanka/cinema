c6de51331f49ae329cb45397a74db1441fa5dbe2
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-09-29 10:27:21 +0530

    feat: header component unit tests created

---
+++ test/components/Header/Header.spec.ts
@@ -0,0 +1,90 @@
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import Header from '@/components/Header/Header.vue'
+
+describe('Header.vue', () => {
+  it('should render header component', () => {
+    const $route = {
+      path: '',
+    }
+    const wrapper = mount(Header, {
+      mocks: {
+        $route,
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    })
+
+    expect(wrapper.find('.header').exists()).toBeTruthy()
+  })
+
+  it('should contain "Back to Movies" text', () => {
+    const $route = {
+      path: '/movies/123',
+    }
+
+    const wrapper = mount(Header, {
+      mocks: {
+        $route,
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    })
+
+    expect(wrapper.find('.header__back-link').text()).toContain(
+      'Back to Movies'
+    )
+  })
+
+  it('should display the "Back to Movies" link when not on the root path', () => {
+    const $route = {
+      path: '/movies/123',
+    }
+
+    const wrapper = mount(Header, {
+      mocks: {
+        $route,
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    })
+
+    expect(wrapper.find('.header__back-link').exists()).toBeTruthy()
+  })
+
+  it('should hide the "Back to Movies" link when on the root path', () => {
+    const $route = {
+      path: '/',
+    }
+
+    const wrapper = mount(Header, {
+      mocks: {
+        $route,
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    })
+
+    expect(wrapper.find('.header__back-link').exists()).toBeFalsy()
+  })
+
+  it('should display the logo image', () => {
+    const $route = {
+      path: '',
+    }
+
+    const wrapper = mount(Header, {
+      mocks: {
+        $route,
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    })
+
+    expect(wrapper.find('.header__logo').exists()).toBeTruthy()
+  })
+})
