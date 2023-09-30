32f6d941bbf7df86f534b4c4fed3d11321509261
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-09-30 09:28:31 +0530

    [P-001]: movie card list component created

---
+++ components/MovieCardList/MovieCardList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Now Showing</h1>
+    <div v-if="movies.length > 0" class="movie-card-list">
+      <MovieCard v-for="movie in movies" :key="movie.title" :movie="movie" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { PropType } from 'vue/types'
+import { MovieCardListProps } from './props'
+import MovieCard from '@/components/MovieCard/MovieCard.vue'
+
+export default Vue.extend({
+  name: 'MovieCardListComponent',
+  components: { MovieCard },
+  props: {
+    movies: {
+      type: Array as PropType<MovieCardListProps['movies']>,
+      required: true,
+      default: () => [],
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import 'movie-card-list.scss';
+</style>
