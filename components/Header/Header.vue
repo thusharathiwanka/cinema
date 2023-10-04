b74c82781374eb368169a432330713a1d0f4bb4d
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-10-05 00:19:47 +0530

    [P-005]: element replacements with new components

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="header">
     <nuxt-link v-if="!isRoot" to="/" class="header__back-link"
-      ><Typography>Back to Movies</Typography></nuxt-link
+      ><Typography color="accent">Back to Movies</Typography></nuxt-link
     >
     <img src="@/assets/images/logo.svg" alt="logo" class="header__logo" />
   </nav>
@@ -9,9 +9,13 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import Typography from '@/components/Typography/Typography.vue'
 
 export default Vue.extend({
   name: 'HeaderComponent',
+  components: {
+    Typography,
+  },
   computed: {
     isRoot() {
       return this.$route.path === '/'
