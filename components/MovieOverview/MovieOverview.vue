34cca515c5ab13e36c42af0dadd2fbac540eedb3
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-10-05 00:15:18 +0530

    [P-005]: movie overview component created

---
+++ components/MovieOverview/MovieOverview.vue
@@ -0,0 +1,76 @@
+<template>
+  <div v-if="movie" class="movie-overview">
+    <MoviePoster :imageUrl="movie.imageUrl" :rating="movie.rating" />
+    <div class="movie-overview__details">
+      <div class="movie-overview__content">
+        <Typography
+          type="h1"
+          :title="movie.title"
+          data-cy="movie-overview__title"
+          >{{ movie.title }}</Typography
+        >
+        <Typography
+          type="h2"
+          color="secondary"
+          :truncate="true"
+          :title="movie.genres"
+          data-cy="movie-overview__subtitle"
+        >
+          <span v-for="(genre, index) in movie.genres" :key="genre">
+            {{ genre }}
+            <span v-if="index + 1 < movie.genres.length">, </span>
+          </span>
+        </Typography>
+        <Typography
+          color="secondary"
+          data-cy="movie-overview__overview-content"
+          >{{ movie.overview }}</Typography
+        >
+      </div>
+      <div class="movie-overview__extra">
+        <Typography color="secondary" data-cy="movie-overview__year">{{
+          movie.year
+        }}</Typography>
+        <Typography color="secondary" data-cy="movie-overview__separator"
+          >&nbsp; • &nbsp;</Typography
+        >
+        <Typography color="secondary" data-cy="movie-overview__runtime">{{
+          getHoursAndMinutesFromMinutes()
+        }}</Typography>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { PropType } from 'vue/types'
+import { MovieOverviewProps } from './props'
+import MoviePoster from '@/components/MoviePoster/MoviePoster.vue'
+import Typography from '@/components/Typography/Typography.vue'
+import { convertMinutesToHoursAndMinutes } from '~/lib/utils/time.util'
+
+export default Vue.extend({
+  name: 'MovieOverviewComponent',
+  components: {
+    MoviePoster,
+    Typography,
+  },
+  props: {
+    movie: {
+      type: Object as PropType<MovieOverviewProps>,
+      required: true,
+      default: null,
+    },
+  },
+  methods: {
+    getHoursAndMinutesFromMinutes() {
+      return convertMinutesToHoursAndMinutes(this.movie.runtime)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import 'movie-overview';
+</style>
