76bae2a861d9b52a133f308faa5419d0ebafb8b0
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-10-08 11:45:07 +0530

    [P-008]: select input component created

---
+++ components/SelectInput/SelectInput.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="select-input">
+    <label class="text-input__label">
+      <Typography>{{ label }}</Typography>
+    </label>
+    <select
+      v-model="selectedValue"
+      class="select-input__area"
+      @change="triggerChangeEvent"
+    >
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { PropType } from 'vue/types'
+import { SelectInputProps } from './props'
+
+export default Vue.extend({
+  name: 'SelectInputComponent',
+  props: {
+    label: {
+      type: String as PropType<SelectInputProps['label']>,
+      default: null,
+    },
+    options: {
+      type: Array as PropType<SelectInputProps['options']>,
+      default: () => [],
+    },
+    value: {
+      type: String as PropType<SelectInputProps['value']>,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      selectedValue: this.value,
+    }
+  },
+  methods: {
+    triggerChangeEvent(event: Event) {
+      if (event && event.target) {
+        const newValue = (event.target as HTMLInputElement).value
+        this.$emit('input', newValue)
+      }
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import 'select-input';
+</style>
