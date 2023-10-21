e9863ed398ae49895f6d59450b17838f13f9908c
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-10-21 22:13:42 +0530

    refactor: header component back link handling moved to CSS

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,12 +1,13 @@
 <template>
   <nav v-cloak class="header">
     <nuxt-link v-if="!isRoot" to="/" class="header__back-link">
-      <Typography v-if="!isTablet" color="accent">Back to Movies</Typography>
+      <Typography color="accent" class="header__back-link--text"
+        >Back to Movies</Typography
+      >
       <img
-        v-else
         src="@/assets/images/back-arrow.svg"
         alt="back"
-        data-cy="header__back-link--arrow"
+        class="header__back-link--img"
       />
     </nuxt-link>
     <img src="@/assets/images/logo.svg" alt="logo" class="header__logo" />
@@ -16,31 +17,16 @@
 <script lang="ts">
 import Vue from 'vue'
 import Typography from '@/components/Typography/Typography.vue'
-import { TABLET_MAX_WIDTH } from '@/lib/constants/viewport.constant'
 
 export default Vue.extend({
   name: 'HeaderComponent',
   components: {
     Typography,
   },
-  data() {
-    return {
-      windowWidth: TABLET_MAX_WIDTH,
-    }
-  },
   computed: {
     isRoot(): boolean {
       return this.$route.path === '/'
     },
-    isTablet(): boolean {
-      return this.windowWidth < TABLET_MAX_WIDTH
-    },
-  },
-  mounted() {
-    window.addEventListener('resize', () => {
-      this.windowWidth = window.innerWidth
-    })
-    this.windowWidth = window.innerWidth
   },
 })
 </script>
