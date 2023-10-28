6118bf0092abf48ceee24c826262c18865145778
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-10-28 15:42:16 +0530

    fix: invalid input enabling next step issue fixed

--- components/MovieBookingForm/MovieBookingForm.vue
+++ components/MovieBookingForm/MovieBookingForm.vue
@@ -97,7 +97,13 @@ export default Vue.extend({
   },
   methods: {
     next() {
-      if (!this.validateSteps()) return
+      if (
+        !this.validateSteps() ||
+        this.emailError.length ||
+        this.mobileNumberError.length ||
+        this.nameError.length
+      )
+        return
       this.activeStep = this.activeStep + 1
       saveDraftBookingForm('activeStep', this.activeStep)
     },
