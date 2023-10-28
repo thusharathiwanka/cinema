fd04b0ca0d9f099dfbd74f10e0c0add6ff90a1ec
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-10-28 21:16:57 +0530

    [P-015]: refactored movie booking summary component

--- components/MovieBookingSummary/MovieBookingSummary.vue
+++ components/MovieBookingSummary/MovieBookingSummary.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="movie" class="booking-summary">
+  <div v-if="!isMovieEmpty" class="booking-summary">
     <Typography type="h1" data-cy="booking-summary__title">
       Booking Summary
     </Typography>
@@ -74,6 +74,11 @@ export default Vue.extend({
     movie: {
       type: Object as PropType<MovieBookingSummaryProps>,
       default: null,
+    },
+  },
+  computed: {
+    isMovieEmpty() {
+      return Object.keys(this.movie).length <= 0
     },
   },
   methods: {
