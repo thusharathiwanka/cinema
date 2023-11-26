894efbe04a1559c5929c571cf850838a12404a8a
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-11-26 21:50:06 +0530

    update: build target set to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,9 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  // Deployment target: https://v2.nuxt.com/docs/configuration-glossary/configuration-target/
+  target: 'static',
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
