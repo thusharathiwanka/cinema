2c79544a4b5cb867041a9848e1372540f6c5d0bf
Thushara Thiwanka <thusharathiwanka.me@gmail.com>
2025-06-02 23:35:54 +0530

    [feat]: supportFile config updates

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:4000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
   },
 })
